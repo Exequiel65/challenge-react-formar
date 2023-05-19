dbeb428653f9649f6415edb04ce9002fcbe60c6d
Exequiel65 <marcos97britos@gmail.com>
2023-05-19 18:59:29 -0300

    add config pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Pokedex',
+        short_name: 'Pokedex',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/path/to/icon.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      plugins: [
+        VitePWA({
+          manifest: {
+            name: 'Nombre de tu aplicación',
+            short_name: 'Nombre corto',
+            theme_color: '#ffffff',
+            icons: [
+              {
+                src: '/path/to/icon.png',
+                sizes: '192x192',
+                type: 'image/png',
+              },
+            ],
+          },
+        }),
+      ],
+    }
+  }
 })
+
+
